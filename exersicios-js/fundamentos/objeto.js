e2df9861aafb5859bf7156e8191f28fc55b17f17
Pedro Henrique <ph90305@gmail.com>
2022-03-14 16:18:06 -0300

    criando objetiv

---
+++ exersicios-js/fundamentos/objeto.js
@@ -0,0 +1,17 @@
+const prod1 = {}
+prod1.nome= "Celular Utra Mega"
+prod1.preco = 4998.90
+prod1["Desconto Legal"] = 0.40 // evitar atributos com espaço
+
+
+console.log(prod1)
+console.log(prod1.nome)
+console.log (prod1["Desconto Legal"])
+
+const prod2 = {
+    nome: "Camisa Polo",
+    preco: 79.90,
+
+}
+
+console.log(prod2)
